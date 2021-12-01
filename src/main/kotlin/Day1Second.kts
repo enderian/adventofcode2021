694c1235ccd86b7e308f7bf1c674ecbead9a4120
Spyros Pagkalos <spyros.pagkalos@n26.com>
2021-12-01 16:50:24 +0100

    Make day 1 more performant

--- src/main/kotlin/Day1Second.kts
+++ src/main/kotlin/Day1Second.kts
@@ -5,9 +5,11 @@ val depths = File("../../../inputs/01.txt").readLines()
     .filter { it.isNotEmpty() }
     .map { it.toInt() }
 
-fun sumAt(p: Int): Int =  depths[p] + depths[p + 1] + depths[p + 2]
-
-print((1..(depths.size - 3)).fold(Pair(0, sumAt(0))) { (total, prev), i ->
-    val s = sumAt(i)
-    Pair(total + if (s > prev) 1 else 0, s)
-}.second)
+print(
+    (1..(depths.size - 3)).fold(
+        Pair(0, depths[0] + depths[1] + depths[2])
+    ) { (total, prev), i ->
+        val s = prev + depths[i + 2] - depths[i - 1]
+        Pair(total + if (s > prev) 1 else 0, s)
+    }.first
+)
