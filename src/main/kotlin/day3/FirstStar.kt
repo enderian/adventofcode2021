c5d6203061a6f3092d362fd07cfb1e1d1ac2e908
Spyridon <spyridon@ender.gr>
2021-12-03 23:24:08 +0100

    Improve day 3's first star

--- src/main/kotlin/day3/FirstStar.kt
+++ src/main/kotlin/day3/FirstStar.kt
@@ -5,16 +5,11 @@ import kotlin.math.pow
 
 fun List<Int>.binaryToInt() = reversed().mapIndexed { i, n -> n * 2.0.pow(i).toInt() }.sum()
 fun List<Int>.binaryInverse() = map { if (it == 0) 1 else 0 }
-fun List<Boolean>.toBinaryList() = map { if (it) 1 else 0 }
 
 fun main() {
     val input = readInput(3).map { it.toCharArray().map { c -> c.digitToInt() } }
-
-    val mostCommonBits = input.drop(1).fold(input.first()) { sum, row ->
-        sum.zip(row) { x, y -> x + y }
-    }.map {
-        it > input.size / 2
-    }.toBinaryList()
+    val mostCommonBits = (0 until input.first().size)
+        .map { i -> input.groupBy { x -> x[i] }.maxByOrNull { x -> x.value.size }?.key ?: 0 }
 
     val leastCommonBits = mostCommonBits.binaryInverse()
     val gamma = mostCommonBits.binaryToInt()
